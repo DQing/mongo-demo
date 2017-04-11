4bf63bc42ce7c7a6acdb2d86502d15cb5926f41e
DQing <15829258737@163.com>
2017-04-11 15:54:35 +0800

    add find method

---
+++ demo3/index.js
@@ -0,0 +1,30 @@
+/*
+* 1.connect.js文件中完成连接数据库的作用
+* 2.schema.js文件中定义了数据模型
+* 3.本文件中引用上述两个文件，并实例化了一组数据
+* 4.使用sava方法保存于数据库中
+* 5.使用find方法将数据库中的数据读取并显示在控制台
+* */
+var mongoose = require('mongoose');
+require('./../demo1/connect');
+require('./schema.js');
+var User = mongoose.model('User');//Book为model name
+mongoose.Promise = global.Promise;
+
+var user = new User({
+    name: 'douqing',
+    age:18,
+    sex:'女'
+});
+
+user.save(function (err) {
+    console.log('save status:', err ? 'failed' : 'success');
+});
+
+User.find({}, function (err,results) {
+    if(err){
+        console.log('error message',err);
+        return;
+    }
+    console.log('results',results);
+});
