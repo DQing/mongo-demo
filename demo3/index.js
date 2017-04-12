2ee58bd9f81edfc227771529dc2d7d739739d034
DQing <15829258737@163.com>
2017-04-12 18:45:30 +0800

    add CURD method

--- demo3/index.js
+++ demo3/index.js
@@ -1,10 +1,9 @@
 /*
-* 1.connect.js文件中完成连接数据库的作用
-* 2.schema.js文件中定义了数据模型
-* 3.本文件中引用上述两个文件，并实例化了一组数据
-* 4.使用sava方法保存于数据库中
-* 5.使用find方法将数据库中的数据读取并显示在控制台
-* */
+ * 1.connect.js文件中完成连接数据库的作用
+ * 2.schema.js文件中定义了数据模型
+ * 3.本文件中引用上述两个文件，并实例化了一组数据
+ * 4.完成对数据库表中数据的增删改查
+ * */
 var mongoose = require('mongoose');
 require('./connect');
 var User = require('./schema.js');
@@ -12,18 +11,38 @@ mongoose.Promise = global.Promise;
 
 var user = new User({
     name: 'douqing',
-    age:18,
-    sex:'女'
+    age: 12,
+    sex: '女'
 });
 
 user.save(function (err) {
     console.log('save status:', err ? 'failed' : 'success');
 });
 
-User.find({}, function (err,results) {
-    if(err){
-        console.log('error message',err);
+User.find({}, function (err, results) {
+    if (err) {
+        console.log('error message', err);
         return;
     }
-    console.log('results',results);
+    console.log('results', results);
+});
+User.findOne({name : 'douqing'},function (err, result) {
+    if(err){
+        console.log('error message:', err);
+    }
+    console.log('result', result);
+});
+
+User.findByIdAndUpdate('58ec4b0090c04916666ce203', {name: 'liji'}, function (err, result) {
+    if (err) {
+        console.log('error message:' + err);
+    }
+    console.log(result);
+});
+
+User.remove({name:'douqing'},function (err, result) {
+    if (err){
+        console.log('error message:' + err);
+    }
+    console.log('remove success');
 });
